cabc7fa0c5d96a28cc7b9f5ad0ef903fed54770c
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-05-02 13:00:31 -0400

    Create webapp_main.py

---
+++ webapp_main.py
@@ -0,0 +1,20 @@
+from browser import bind, worker, window
+
+jQuery = window.jQuery
+q_worker = worker.Worker('worker')
+
+def finished_input(text):
+    worker.postMessage(text)
+    term.set_prompt('')
+    term.disable()
+
+term = jQuery('#terminal').terminal(finished_input, {'greetings': '', 'prompt': ''})
+
+@bind(q_worker, "message")
+def output(e):
+    if e.data.type = 'print':
+        term.echo(e.data.text, {'newline': False})
+    else:
+        term.enable()
+        term.set_prompt(e.data.text)
+        
